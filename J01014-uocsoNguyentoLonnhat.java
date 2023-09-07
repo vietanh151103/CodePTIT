b39a2436f6aa620e06fed50991b1fca7f306c0eb
vietanh151103 <133160738+vietanh151103@users.noreply.github.com>
2023-09-07 11:29:30 +0700

    Update and rename uocsoNguyentoLonnhat.java to J01014-uocsoNguyentoLonnhat.java

--- J01014-uocsoNguyentoLonnhat.java
+++ J01014-uocsoNguyentoLonnhat.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package CodePTIT;
 //J01014
 
 import java.util.Scanner;
-/**
- *
- * @author User
- */
+
 public class uocsoNguyentoLonnhat {
     public static Scanner sc = new Scanner(System.in);
     public static boolean ktraSoNguyenTo (long k) {
