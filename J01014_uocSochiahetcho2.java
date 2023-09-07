ae06c0da9ffe6d187082de7f14dd143f5d327193
vietanh151103 <133160738+vietanh151103@users.noreply.github.com>
2023-09-07 11:26:38 +0700

    Add files via upload

---
+++ J01014_uocSochiahetcho2.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vieta
+ */
+public class J01014_uocSochiahetcho2 {
+    
+}
